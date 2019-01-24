67032544f5a93c6df00e94f3f7120c26c9ffedd3
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-01-24 02:14:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/reversename/:name' do 
+<<<<<<< HEAD
     @new = params[:name].reverse
+=======
+    @new = params[:name].split().reverse.join
+>>>>>>> 68e07e4ff52f49d4affb804707fcf21dac550b57
     erb :reversename
   end
 
@@ -12,16 +16,25 @@ class App < Sinatra::Base
   end
   
   get '/say/:number/:phrase' do 
+<<<<<<< HEAD
     @words = params[:phrase] * (params[:number].to_i)
+=======
+    @words = params[:phase] * params[:number]
+>>>>>>> 68e07e4ff52f49d4affb804707fcf21dac550b57
     erb :say 
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
+<<<<<<< HEAD
     @word_list = params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4] + " " + params[:word5] + "."
+=======
+    @word_list = params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5]
+>>>>>>> 68e07e4ff52f49d4affb804707fcf21dac550b57
     erb :say
   end
     
   get '/:operation/:number1/:number2' do 
+<<<<<<< HEAD
     if params[:operation] == "add"
       @answer = (params[:number1].to_i) + (params[:number2].to_i)
     elsif params[:operation] == "subtract"
@@ -30,6 +43,22 @@ class App < Sinatra::Base
       @answer = (params[:number1].to_i) * (params[:number2].to_i)
     elsif params[:operation] == "divide"
       @answer = (params[:number1].to_i) / (params[:number2].to_i)     
+=======
+    if params[:operation] == "+"
+      @answer = (params[:number1].to_i) + (params[:number2].to_i)
+    elsif params[:operation] == "-"
+      @answer = (params[:number1].to_i) - (params[:number2].to_i)
+    elsif params[:operation] == "*"
+      @answer = (params[:number1].to_i) * (params[:number2].to_i)
+    elsif params[:operation] == "/"
+      @answer = (params[:number1].to_i) / (params[:number2].to_i)
+    elsif params[:operation] == "**"
+      @answer = (params[:number1].to_i) ** (params[:number2].to_i)
+    elsif params[:operation] == "%"
+      @answer = (params[:number1].to_i) % (params[:number2].to_i)  
+    else 
+      @answer = "The operation can not be completed."
+>>>>>>> 68e07e4ff52f49d4affb804707fcf21dac550b57
     end
     erb :index 
   end
